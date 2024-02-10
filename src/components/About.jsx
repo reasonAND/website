a262686fc58ff1e470182c24dae3ee16a3b811bf
Dwaipayan Dutta <dwaipayandutta.official@gmail.com>
2024-02-11 00:07:09 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,10 @@ const About = () => {
     const locomotiveScroll = new LocomotiveScroll();  
 
   return (
-    <div className="w-[100vw] h-[100vh] p-3 text-[#ffce00] bg-[#006450] text-[20px] text-center">
+    <div className="w-[100vw] h-[100vh] bg-[#292551]">
+      <div className="w-[100vw] h-[100vh] p-3 text-[#ffce00] bg-[#006450] text-[20px] text-center rounded-3xl">
         About
+      </div>
     </div>
   )
 }
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,6 +1,7 @@
 const Showcase = () => {
   return (
-    <div className="w-[100vw] h-[100vh] bg-[#292551] text-[#df5e4d] text-[20px] text-center">
+    <div className="w-[100vw] h-[100vh] bg-[#242424]">
+      <div className="w-[100vw] h-[100vh] bg-[#292551] text-[#df5e4d] text-[20px] text-center rounded-t-3xl">
       {/* <div className="h-[23.5%] flex items-center">
         <h1 className="h-[82%] w-full flex justify-center items-center bg-slate-50 text-[25vh]">
           LOREM
@@ -9,6 +10,7 @@ const Showcase = () => {
       <div className="h-[76.5%] bg-green-900 flex items-center">
         Showcase
       </div> */}
+      </div>
     </div>
   )
 }
