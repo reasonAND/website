869f77294a5d44703f93ef9d617b71671c50d129
Dwaipayan Dutta <dwaipayandutta.official@gmail.com>
2024-02-15 23:22:00 +0530

    fix]

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,12 @@ import { useLayoutEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getRandomPastelColor() {
+  const hue = Math.floor(Math.random() * 360);
+  const pastelSaturation = Math.floor(Math.random() * 20) + 80; // Saturation range 80 - 100
+  const pastelLightness = Math.floor(Math.random() * 20) + 70; // Lightness range 70 - 90
+
+  return `hsl(${hue},${pastelSaturation}%,${pastelLightness}%)`;
 }
 
 export default function Scene() {
@@ -50,7 +49,7 @@ export default function Scene() {
               <div
               key={index}
               className={`panel min-w-[100vw] h-screen p-5 text-[20px] text-center`}
-              style={{ backgroundColor: getRandomColor() }}>
+              style={{ backgroundColor: getRandomPastelColor() }}>
                 {panel}
               </div>
           ))
